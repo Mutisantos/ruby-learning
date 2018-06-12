e4423aecc26483f4ccb15878a5062fe8fad0daa5
Esteban Hernández <mutisantos@hotmail.com>
2018-06-12 13:10:51 -0500

    Detalles del uso de heroku

--- App/Application.rb
+++ App/Application.rb
@@ -4,10 +4,14 @@
 # jruby -S gem install bundler
 # gem install bundler
 # Instalar openSSL y luego: gem install puma -- --with-opt-dir=c:\openssl
+
 # Instalar el Cliente de Heroku (CLI)
 # heroku login
 # crear un Gemfile que tenga en cuenta las gemas de la aplicación, puma y openssl
 # bundle install 
 # iniciar un repositorio en git y darle commit a los archivos del app
 # heroku create <nombre-app>
-# 
+# En caso que no se añada el git remote hacer:
+
+# git remote add heroku <url-del-repo-creado-en-heroku>:https://dashboard.heroku.com/apps/ruby-rack-web-mutisantos/settings
+# git push heroku master : Despliega la aplicacion del repositorio dado en heroku
