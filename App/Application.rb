bb8578441a34bf74dc94c9aa21cd9f2057669989
Esteban Hernández <mutisantos@hotmail.com>
2018-06-12 11:50:47 -0500

    Creación de aplicación a desplegar

--- App/Application.rb
+++ App/Application.rb
@@ -1,7 +1,10 @@
-#gem install rack
-#La gema permite hacer apps web sencillas
-
-
-
-
-
+# Usar Heroku como plataforma de despliegue de aplicaciones
+# En windows https://devcenter.heroku.com/articles/getting-started-with-jruby#introduction
+# Rails en windows hasta el paso 2: http://guides.railsgirls.com/install#setup-for-windows
+# jruby -S gem install bundler
+# gem install bundler
+# Instalar openSSL y luego: gem install puma -- --with-opt-dir=c:\openssl
+# Instalar el Cliente de Heroku (CLI)
+# heroku login
+# crear un Gemfile que tenga en cuenta las gemas de la aplicación, puma y openssl
+# bundle install puma
